50bcaba4f94c879e077d3ec6c4512f4a5a2e8c36
sprawesh <sprawesh@hotmail.com>
2014-01-12 18:28:31 -0500

    calculation of sum

--- SalesTool/src/in/demo/salestool/SalesData.java
+++ SalesTool/src/in/demo/salestool/SalesData.java
@@ -1,13 +1,20 @@
 package in.demo.salestool;
 
 public class SalesData {
+	
+	/**
+	 * represents the sales data for a given period
+	 */
 	int data[] = {0, 4, 42};
 	
 	
 	public void display() {		
 		System.out.println("Data");
+		int sum = 0;
 		for (int i = 0; i < data.length; i++) {
 			System.out.println("Next value: " + data[i]); 
+			sum += i;
 		}
+		System.out.println("sum : " + sum); 
 	}
 }
